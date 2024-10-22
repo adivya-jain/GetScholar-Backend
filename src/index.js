36470ce9192f14284261ba8a356739e37d2ab912
Adivya Jain <95540661+adivya-jain@users.noreply.github.com>
2024-10-22 04:52:58 +0000

    Post for adding new Scholarship data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 const app = express()
+app.use(express.json())
 import { Scholarship } from "./models/scholarship.model.js"
 dotenv.config({
    path: "./.env",
@@ -20,5 +21,51 @@ app.get("/all-scholarship", async (req, res) => {
       res.status(200).json(scholarships)
    } catch (error) {
       res.status(500).json({ message: "Error Fetching data", error })
+   }
+})
+
+app.post("/addscholarship", async (req, res) => {
+   try {
+      const {
+         title,
+         description,
+         eligibilityCriteria,
+         amount,
+         deadline,
+         applicationLink,
+         imageUrl,
+      } = req.body
+
+      // Check if all required fields are provided
+      if (
+         !title ||
+         !description ||
+         !eligibilityCriteria ||
+         !amount ||
+         !deadline ||
+         !applicationLink
+      ) {
+         return res
+            .status(400)
+            .json({ message: "All required fields must be provided" })
+      }
+
+      // Create a new scholarship entry
+      const newScholarship = await Scholarship.create({
+         title,
+         description,
+         eligibilityCriteria,
+         amount,
+         deadline,
+         applicationLink,
+         imageUrl, // optional
+      })
+
+      res.status(201).json(newScholarship)
+   } catch (error) {
+      res.status(500).json({
+         message: "Error adding scholarship data",
+         error: error.message,
+      })
    }
 })
